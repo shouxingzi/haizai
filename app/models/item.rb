2a1817ef7b77b8b6ccef7a6fa7d9e058fd9317c7
shouxingzi <79133015+shouxingzi@users.noreply.github.com>
2021-05-02 15:01:47 +0900

    itemモデルバリテーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,11 @@ class Item < ApplicationRecord
   has_one :city through :prefecture
   has_many_attached :images
 
+  with_options presence: true do
+    validates  :item
+    validates  :description
+    validates  :prefecture_id
+    validates  :city_id
+    validates  :user_id
+  end
 end
