fb1f5d38943acf7a9c1f740cecff3637e9a1fe71
shouxingzi <79133015+shouxingzi@users.noreply.github.com>
2021-05-05 22:44:39 +0900

    user show実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # post 'tweets', to: 'tweets#cities_select'
 
   resources :tweets, only: [:new, :create, :show, :edit, :update, :destroy] 
+  resources :users, only: :show
 end
